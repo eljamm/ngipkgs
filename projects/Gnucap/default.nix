1c1a4a6c90048f8c4bdc25593772de9c17aa5e88
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-05 15:25:47 +0100

    feat: add Gnucap project
    From ngi-nix/ngipkgs/issues/541

---
+++ projects/Gnucap/default.nix
@@ -0,0 +1,66 @@
+{
+  lib,
+  pkgs,
+  sources,
+}@args:
+{
+  name = "Gnucap";
+  metadata.subgrants = [
+    "Gnucap-MixedSignals"
+    "Gnucap-VerilogAMS"
+  ];
+  references = {
+    wiki = {
+      text = "Gnucap Wiki";
+      link = "http://gnucap.org/dokuwiki/doku.php?id=gnucap:start";
+    };
+    manual = {
+      text = "User Manual (PDF)";
+      link = "https://www.gnu.org/software/gnucap/gnucap-man.pdf";
+    };
+    notes = {
+      text = "Notes for Developers";
+      link = "http://gnucap.org/dokuwiki/doku.php/gnucap:manual:tech";
+    };
+  };
+  nixos = {
+    modules.programs.gnucap = {
+      module =
+        { pkgs, ... }:
+        {
+          packages = with pkgs; [
+            gnucap
+            gnucap-full
+          ];
+        };
+      # http://gnucap.org/dokuwiki/doku.php/gnucap:user:build_system_for_plugins
+      # http://gnucap.org/dokuwiki/doku.php/gnucap:user:command_plugins
+      plugins = null;
+      examples.base = {
+        module = { ... }: { };
+        description = "Basic configuration, mainly used for testing purposes.";
+        tests.base = null;
+        references = {
+          tutorial = {
+            text = "Examples, tutorial";
+            link = "http://gnucap.org/dokuwiki/doku.php/gnucap:manual:examples";
+          };
+          manual = {
+            text = "Gnucap manual";
+            link = "http://gnucap.org/dokuwiki/doku.php/gnucap:manual";
+          };
+        };
+      };
+      references = {
+        build = {
+          text = "Installation Instructions";
+          link = "https://git.savannah.gnu.org/cgit/gnucap.git/tree/INSTALL";
+        };
+        tests = {
+          text = "Running Tests";
+          link = "https://git.savannah.gnu.org/cgit/gnucap.git/tree/tests/README";
+        };
+      };
+    };
+  };
+}
