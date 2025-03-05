36da00b02fdee4cc07346910066cc7814621626b
eljamm <fedi.jamoussi@protonmail.ch>
2025-03-05 10:23:07 +0100

    fix: expected a set but found null

--- default.nix
+++ default.nix
@@ -74,20 +74,18 @@ rec {
 
           get = func: attrs: lib.concatMapAttrs (_: value: func value) attrs;
 
-          examplesFrom = value: lib.mapAttrs (_: example: { path = example.module; }) value.examples;
+          examplesFrom = value: lib.mapAttrs (_: example: { path = example.module or {}; }) value.examples;
           testsFrom = value: lib.concatMapAttrs (_: example: example.tests or { }) value.examples;
         in
         {
           packages = { }; # NOTE: the overview expects a set
           nixos.modules.services = lib.mapAttrs (name: value: value.module) services;
           nixos.examples =
-            new-project.nixos.examples or { }
-            // new-project.nixos.examples or { }
+            (empty-if-not-attrs new-project.nixos.examples or {})
             // get examplesFrom services
             // get examplesFrom programs;
           nixos.tests =
-            new-project.nixos.tests or { }
-            // new-project.nixos.tests or { }
+            (empty-if-not-attrs new-project.nixos.tests or {})
             // get testsFrom services
             // get testsFrom programs;
         };
