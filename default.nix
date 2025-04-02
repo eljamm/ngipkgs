2b418566eddda336886083c280a5d41ab58d103e
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-02 16:59:01 +0200

    refactor: create vm script with ngipkgs.demo

--- default.nix
+++ default.nix
@@ -270,71 +270,74 @@ rec {
           }
           // args
         );
-    in
-    nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        module
-        (sources.nixpkgs + "/nixos/modules/profiles/qemu-guest.nix")
-        (sources.nixpkgs + "/nixos/modules/virtualisation/qemu-vm.nix")
-        (
-          { config, ... }:
-          {
-            users.users.nixos = {
-              isNormalUser = true;
-              extraGroups = [ "wheel" ];
-              initialPassword = "nixos";
-            };
-
-            users.users.root = {
-              initialPassword = "root";
-            };
-
-            security.sudo.wheelNeedsPassword = false;
-
-            services.getty.autologinUser = "nixos";
-            services.getty.helpLine = ''
-
-              Welcome to Ngipkgs!
-            '';
-
-            virtualisation = {
-              memorySize = 4096;
-              cores = 4;
-              graphics = false;
-              diskImage = null;
-
-              qemu.options = [
-                "-cpu host"
-                "-enable-kvm"
-              ];
-
-              # ssh + open service ports
-              forwardPorts = map (port: {
-                from = "host";
-                guest.port = port;
-                host.port = port;
-                proto = "tcp";
-              }) config.networking.firewall.allowedTCPPorts;
-            };
-
-            services.openssh = {
-              enable = true;
-              ports = lib.mkDefault [ 2222 ];
-              settings = {
-                PasswordAuthentication = true;
-                PermitEmptyPasswords = "yes";
-                PermitRootLogin = "yes";
+      system = nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          module
+          (sources.nixpkgs + "/nixos/modules/profiles/qemu-guest.nix")
+          (sources.nixpkgs + "/nixos/modules/virtualisation/qemu-vm.nix")
+          (
+            { config, ... }:
+            {
+              users.users.nixos = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" ];
+                initialPassword = "nixos";
               };
-            };
 
-            system.stateVersion = "25.05";
+              users.users.root = {
+                initialPassword = "root";
+              };
 
-            networking.firewall.enable = false;
-          }
-        )
-      ] ++ extendedNixosModules;
-    };
+              security.sudo.wheelNeedsPassword = false;
+
+              services.getty.autologinUser = "nixos";
+              services.getty.helpLine = ''
+
+                Welcome to Ngipkgs!
+              '';
+
+              virtualisation = {
+                memorySize = 4096;
+                cores = 4;
+                graphics = false;
+                diskImage = null;
+
+                qemu.options = [
+                  "-cpu host"
+                  "-enable-kvm"
+                ];
+
+                # ssh + open service ports
+                forwardPorts = map (port: {
+                  from = "host";
+                  guest.port = port;
+                  host.port = port;
+                  proto = "tcp";
+                }) config.networking.firewall.allowedTCPPorts;
+              };
+
+              services.openssh = {
+                enable = true;
+                ports = lib.mkDefault [ 2222 ];
+                settings = {
+                  PasswordAuthentication = true;
+                  PermitEmptyPasswords = "yes";
+                  PermitRootLogin = "yes";
+                };
+              };
+
+              system.stateVersion = "25.05";
+
+              networking.firewall.enable = false;
+            }
+          )
+        ] ++ extendedNixosModules;
+      };
+    in
+    pkgs.writeShellScript "demo-vm" ''
+      ${system.config.system.build.vm}/bin/run-nixos-vm
+    '';
 
   # TODO: find a better place for the demo file
   demo-system = import ./overview/demo.nix {
