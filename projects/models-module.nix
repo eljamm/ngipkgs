53aaaf7cd87f5329a8ff90e86261c1fdc4d53e58
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-30 18:00:16 +0200

    models: init typing with module system

---
+++ projects/models-module.nix
@@ -0,0 +1,85 @@
+{
+  lib,
+  pkgs,
+  sources,
+  ...
+}:
+let
+  inherit (lib)
+    types
+    mkOption
+    ;
+
+  mkProject = name: value: {
+    options.projects."${name}" = {
+      name = mkOption {
+        type = with types; nullOr str;
+        default = null;
+      };
+      metadata = mkOption {
+        type =
+          with types;
+          nullOr (submodule {
+            options = {
+              summary = mkOption {
+                type = nullOr str;
+                default = null;
+              };
+              # TODO: convert all subgrants to `subgrantType`, remove listOf
+              subgrants = mkOption {
+                type = either (listOf str) subgrantType;
+                default = null;
+              };
+              links = mkOption {
+                type = attrsOf urlType;
+                default = { };
+              };
+            };
+          });
+        default = null;
+      };
+      binary = mkOption {
+        type = with types; attrsOf binaryType;
+        default = { };
+      };
+      nixos = mkOption {
+        type =
+          with types;
+          submodule {
+            options = {
+              services = mkOption {
+                type = nullOr (attrsOf (nullOr serviceType));
+                default = null;
+              };
+              programs = mkOption {
+                type = nullOr (attrsOf (nullOr programType));
+                default = null;
+              };
+              # An application component may have examples using it in isolation,
+              # but examples may involve multiple application components.
+              # Having examples at both layers allows us to trace coverage more easily.
+              # If this tends to be too cumbersome for package authors and we find a way obtain coverage information programmatically,
+              # we can still reduce granularity and move all examples to the application level.
+              examples = mkOption {
+                type = nullOr (attrsOf exampleType);
+                default = null;
+              };
+              # TODO: Tests should really only be per example, in order to clarify that we care about tested examples more than merely tests.
+              #       But reality is such that most NixOS tests aren't based on self-contained, minimal examples, or if they are they can't be extracted easily.
+              #       Without this field, many applications will appear entirely untested although there's actually *some* assurance that *something* works.
+              #       Eventually we want to move to documentable tests exclusively, and then remove this field, but this may take a very long time.
+              tests = mkOption {
+                type = nullOr (attrsOf testType);
+                default = null;
+              };
+            };
+          };
+      };
+    };
+
+    config.projects."${name}" = value;
+  };
+in
+{
+  inherit mkProject;
+}
