3f54b3f92fa017306a03cf9816ac17b13731f32d
eljamm <fedi.jamoussi@protonmail.ch>
2025-04-30 18:10:33 +0200

    models: add library type

--- projects/models-module.nix
+++ projects/models-module.nix
@@ -130,6 +130,14 @@ let
       };
     };
 
+  # TODO: plugins are actually component *extensions* that are of component-specific type,
+  #       and which compose in application-specific ways defined in the application module.
+  #       we can't express that with yants, but with the module system, which gives us a bit of dependent typing.
+  #       this also means that there's no fundamental difference between programs and services,
+  #       and even languages: libraries are just extensions of compilers.
+  # TODO: implement this, now that we're using the module system
+  pluginType = with types; anything;
+
   moduleType =
     with types;
     oneOf [
