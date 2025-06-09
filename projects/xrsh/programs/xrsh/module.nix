28deddd2fc609f111b7c28af6d95a918e9d976f4
themadbit <marktanui75@gmail.com>
2025-06-09 14:56:21 +0300

    make serving port optional

--- projects/xrsh/programs/xrsh/module.nix
+++ projects/xrsh/programs/xrsh/module.nix
@@ -11,11 +11,22 @@ in
   options.programs.xrsh = {
     enable = lib.mkEnableOption "enable xrsh";
     package = lib.mkPackageOption pkgs "xrsh" { };
+    port = lib.mkOption {
+      description = ''
+        Port to serve xsrsh on
+        Default port is :8080
+      '';
+      type = lib.types.nullOr lib.types.port;
+      default = 8080;
+    };
   };
 
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       cfg.package
     ];
+    environment.variables = {
+      XRSH_PORT = toString cfg.port;
+    };
   };
 }
