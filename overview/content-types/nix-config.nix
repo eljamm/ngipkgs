28de49c79b00328012c3c2835aada46ef5a9a787
eljamm <fedi.jamoussi@protonmail.ch>
2025-06-09 23:49:28 +0200

    overview: re-use nix config module

--- overview/content-types/nix-config.nix
+++ overview/content-types/nix-config.nix
@@ -1,29 +1,73 @@
 {
-  name,
-  config,
+  pkgs,
+  options,
   lib,
   ...
 }:
 let
   inherit (lib) mkOption types;
+
+  nixOpts = options.nix.settings.type.getSubOptions { };
+
+  nixConfig =
+    cfg:
+    with lib;
+    let
+      mkValueString =
+        v:
+        if v == null then
+          ""
+        else if isInt v then
+          toString v
+        else if isBool v then
+          boolToString v
+        else if isFloat v then
+          floatToString v
+        else if isList v then
+          toString v
+        else if isDerivation v then
+          toString v
+        else if builtins.isPath v then
+          toString v
+        else if isString v then
+          v
+        else if strings.isConvertibleWithToString v then
+          toString v
+        else
+          abort "The nix conf value: ${toPretty { } v} can not be encoded";
+
+      mkKeyValue = k: v: "${escape [ "=" ] k} = ${mkValueString v}";
+
+      mkKeyValuePairs = attrs: concatStringsSep "\n" (mapAttrsToList mkKeyValue attrs);
+
+      isExtra = key: hasPrefix "extra-" key;
+    in
+    pkgs.writeTextFile {
+      name = "nix.conf";
+      text = ''
+        ${mkKeyValuePairs (filterAttrs (key: value: !(isExtra key)) cfg.settings)}
+        ${mkKeyValuePairs (filterAttrs (key: value: isExtra key) cfg.settings)}
+      '';
+    };
 in
 {
   options = {
     __toString = mkOption {
       type = with types; functionTo str;
       readOnly = true;
-      default =
-        self:
-        let
-          separator = "'$'\\n''";
-          concatSettings = lib.concatStringsSep separator (
-            lib.map (attrs: "${attrs.name} = ${toString attrs.value}") self.settings
-          );
-        in
-        "NIX_CONFIG='${concatSettings}'";
+      default = self: "NIX_CONFIG='${lib.trim (nixConfig self).text}'";
     };
     settings = mkOption {
-      type = with types; listOf (submodule ./config-item.nix);
+      type =
+        with types;
+        submodule {
+          options = {
+            inherit (nixOpts)
+              substituters
+              trusted-public-keys
+              ;
+          };
+        };
     };
   };
 }
