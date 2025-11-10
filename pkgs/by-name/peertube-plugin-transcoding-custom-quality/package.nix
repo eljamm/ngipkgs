3b0d34f266a70a6adb5a77194d5ecf9b60ada48f
Fedi Jamoussi <Fedi.Jamoussi@protonmail.ch>
2025-11-10 23:07:36 +0000

    peertube-plugin-transcoding-custom-quality: 0-unstable-2025-05-30 -> 0-unstable-2025-10-29

--- pkgs/by-name/peertube-plugin-transcoding-custom-quality/package.nix
+++ pkgs/by-name/peertube-plugin-transcoding-custom-quality/package.nix
@@ -6,15 +6,15 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "peertube-plugin-transcoding-custom-quality";
-  version = "0-unstable-2025-05-30";
+  version = "0-unstable-2025-10-29";
 
   src = fetchFromGitLab {
     domain = "framagit.org";
     owner = "framasoft";
     repo = "peertube/official-plugins";
-    rev = "1c6f794d7a5d9c69374cb6fa1daf184258acb63a";
+    rev = "b6ee1eee69f3e7ffd951c354b39f22207f500f57";
     sparseCheckout = [ "peertube-plugin-transcoding-custom-quality" ];
-    hash = "sha256-DEYyrQtogtKCSZM8nIBZiKIcbbG22RmXsenGPd9hTGE=";
+    hash = "sha256-m+oFaX9/8MyDAUt4omsdliAR5ZHFJzfeGkuxufWZTjM=";
   };
 
   sourceRoot = "${finalAttrs.src.name}/peertube-plugin-transcoding-custom-quality";
