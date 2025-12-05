163457c20088d49689bec5325dd466ac53265fc7
eljamm <fedi.jamoussi@protonmail.ch>
2025-12-05 07:57:46 +0100

    Move systemAgnostic config up in flake attrs

--- maintainers/flake/default.nix
+++ maintainers/flake/default.nix
@@ -5,6 +5,24 @@
   ...
 }:
 {
+  # system-independant (e.g. nixosModules)
+  systemAgnostic = {
+    lib = default.overlays.customLib null null;
+
+    nixosConfigurations = {
+      makemake = import ../../infra/makemake { inputs = default.sources; };
+    };
+
+    toplevel = machine: machine.config.system.build.toplevel; # for makemake
+
+    # WARN: this is currently unstable and subject to change in the future
+    nixosModules = default.nixos-modules;
+
+    # Overlays a package set (e.g. Nixpkgs) with the packages defined in this flake.
+    overlays.default = default.overlays.default;
+  };
+
+  # depends on the system (e.g. packages.x86_64-linux)
   perSystem = rec {
     packages = default.ngipkgs // {
       inherit (default) overview demos;
@@ -39,21 +57,5 @@
         pre-commit run --all-files
       '';
     };
-  };
-
-  systemAgnostic = {
-    lib = default.overlays.customLib null null;
-
-    nixosConfigurations = {
-      makemake = import ../../infra/makemake { inputs = default.sources; };
-    };
-
-    toplevel = machine: machine.config.system.build.toplevel; # for makemake
-
-    # WARN: this is currently unstable and subject to change in the future
-    nixosModules = default.nixos-modules;
-
-    # Overlays a package set (e.g. Nixpkgs) with the packages defined in this flake.
-    overlays.default = default.overlays.default;
   };
 }
